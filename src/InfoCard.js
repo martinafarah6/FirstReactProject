24537e54d44d952a98d7c5efea4b2de17ecd9c15
martinafarah6 <martinafarah6@gmail.com>
2026-08-29 13:24:19 +0300

    Update image source to use environment variable

--- src/InfoCard.js
+++ src/InfoCard.js
@@ -2,7 +2,7 @@ function InfoCard()
 {
     return(
         <div className="info-card">
-            <img src="/myportfolio.jpg" alt="Martina Farah" />
+            <img src={`${process.env.PUBLIC_URL}/myportfolio.jpg`}alt="Martina Farah" />
             <h1>Martina Farah</h1>
             <h2 className="age">20 Years Old</h2>
         </div>
