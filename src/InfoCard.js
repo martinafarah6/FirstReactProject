aaccffa58bbd8285e4f35fee284e69225debd3ce
martinafarah6 <martinafarah6@gmail.com>
2026-08-29 13:04:35 +0300

    Add InfoCard component for displaying user info

---
+++ src/InfoCard.js
@@ -0,0 +1,10 @@
+function InfoCard()
+{
+    return(
+        <div className="info-card">
+            <h1>Martina Farah</h1>
+            <h2 className="age">20 Years Old</h2>
+        </div>
+    )
+}
+export default InfoCard
