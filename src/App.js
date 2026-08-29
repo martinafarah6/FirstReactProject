25a427cf69c798f0ce7fc5d173c55b9d5d1600c6
martinafarah6 <martinafarah6@gmail.com>
2026-08-29 13:02:52 +0300

    Create App component with InfoCard and SkillCard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import InfoCard from "./InfoCard"
+import SkillCard from "./SkillCard"
+import "./App.css"
+function App()
+{
+    return(
+        <>
+            <InfoCard/>
+            <SkillCard/>
+        </>
+    )
+}
+
+export default App
