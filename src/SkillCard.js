a64a033ca2068c665e8096294063b0759313fd33
martinafarah6 <martinafarah6@gmail.com>
2026-08-29 13:05:02 +0300

    Add SkillCard component with skills list

---
+++ src/SkillCard.js
@@ -0,0 +1,16 @@
+function SkillCard()
+{
+    return (
+        <div className="skill-card">
+            <h2>Skills </h2>
+            <div className="skills-list">
+                <p>Html</p>
+                <p>CSS</p>
+                <p>JavaScript</p>
+                <p>C++</p>
+                <p>problem solving</p>
+            </div>
+        </div>
+    )
+}
+export default SkillCard
